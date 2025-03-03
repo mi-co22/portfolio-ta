54c927ce191b6f30300d1fef50f7328f4b43d414
mi-co22 <rgn.mico.main@gmail.com>
2025-03-03 17:12:23 +0900

    prettier追加。パスエイリアスの設定。画像の適正な読み込み。

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+import path from "path";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(process.cwd(), "src"), // ← "src" を基準にする
+    };
+    return config;
+  },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+};
+
+// module.exports = nextConfig; // これは削除
+export default nextConfig; // こちらを使用
